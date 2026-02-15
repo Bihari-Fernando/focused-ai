5403e826fad37e9f1e8f318819d903a53c4300f6
Bihari-Fernando <biharifernando2000@gmail.com>
2026-02-15 18:57:27 +0530

    create mood-form.tsx file

---
+++ components/mood/mood-form.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
+import { Loader2 } from "lucide-react";
+
+import { useRouter } from "next/navigation";
