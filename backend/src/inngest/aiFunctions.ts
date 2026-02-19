951155bb0e0503ab8ff4b7c2bafca4438cf57a42
Bihari-Fernando <biharifernando2000@gmail.com>
2026-02-19 08:38:43 +0530

    create aiFunctions.ts file

---
+++ backend/src/inngest/aiFunctions.ts
@@ -0,0 +1,34 @@
+import { inngest } from "./index";
+import { GoogleGenAI } from "@google/genai";
+
+const genAI = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY || 'AIzaSyDIaNP8hRN6dTU28b9mi5nl_JAGvSaAHZ4'
+});
+
+export const processChatMessage = inngest.createFunction(
+    {
+        id: "process-chat-message",
+    },
+    { event: "therapy/session.message" },
+    async ({ event, step }) => {
+        try {
+            const {
+                message,
+                history,
+                memory = {
+                    userProfile: {
+                        emotionalState: [],
+                        riskLevel: 0,
+                        preferences: {},
+                    },
+                    sessionContext: {
+                        conversationThemes: [],
+                        currentTechnique: null,
+                    },
+                },
+                goals = [],
+                systemPrompt,
+            } = event.data;
+        } catch (error) { }
+    }
+);
